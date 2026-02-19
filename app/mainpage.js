756fcbfd1a303d0ba9fb99a588dc8c18efc75943
Rama Mustofa <mustofarama61@gmail.com>
2026-02-19 11:29:35 +0700

    updateee

---
+++ app/mainpage.js
@@ -0,0 +1,47 @@
+Ext.define("TDK.mainpage", {
+  extend: "Ext.panel.Panel",
+  id: "mainpage",
+  alias: "widget.mainpage",
+  pid: "mainpage",
+
+  requires: [
+    "TDK.Cmainpage",
+    "TDK.formm.form",
+    "TDK.Grid",
+    "TDK.gaji"
+  ],
+
+  controller: "Cmainpage",
+  items: [
+    {
+      xtype: 'Grid'
+    }
+  ],
+  dockedItems: [
+        {
+          xtype: "toolbar",
+          height: 30,
+          dock: "top",
+          items: [
+            "-",
+            { 
+              text: "Refresh", 
+              icon: vconfig.getstyle + "/icon/refresh.gif ",
+              handler: "onRefresh"
+            },
+            "-",
+            { 
+              text: "New Input", 
+              handler: "onNewInput",  // ← Handler untuk buka FROMpo
+              icon: vconfig.getstyle + "/icon/add.png"
+            },
+            "-",
+            { 
+              text: "Delete", 
+              handler: "onNewInput",  // ← Handler untuk buka FROMpo
+              icon: vconfig.getstyle + "/icon/add.png"
+            },
+          ]
+        }
+      ],
+});
